4106283d122d0fde450286369833466adc2bca96
Varshith Reddy <varshithreddybokka4444@gmail.com>
2026-08-01 12:22:02 +0530

    Allow requests from vercel frontend

--- backend/src/main/java/com/codingshuttle/youtube/hospitalManagement/config/WebConfig.java
+++ backend/src/main/java/com/codingshuttle/youtube/hospitalManagement/config/WebConfig.java
@@ -1,6 +1,5 @@
 package com.codingshuttle.youtube.hospitalManagement.config;
 
-
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -11,7 +10,10 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:63342")
+                .allowedOrigins(
+                        "http://localhost:63342",
+                        "https://hospital-management-system-6h9x.vercel.app"
+                )
                 .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE")
                 .allowedHeaders("*");
     }
